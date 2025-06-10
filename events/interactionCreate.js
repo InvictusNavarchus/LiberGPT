efae08948e498a78b8a14b41d0a756c8437cdab9
Invictus Navarchus <vm045hfydfn4gqmvvfd6@protonmail.com>
2025-06-10 20:44:59 +0700

    feat: implement safeReply helper for handling Discord interactions with error management

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,4 +1,5 @@
 import { Events, MessageFlags } from 'discord.js';
+import safeReply from '../helpers/safeReply.js';
 
 export default {
 	name: Events.InteractionCreate,
@@ -11,16 +12,12 @@ export default {
 			console.error(`No command matching ${interaction.commandName} was found.`);
 			return;
 		}
-
-		try {
+		
+		await safeReply(interaction, async () => {
 			await command.execute(interaction);
-		} catch (error) {
-			console.error(error);
-			if (interaction.replied || interaction.deferred) {
-				await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-			} else {
-				await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-			}
-		}
+		}, {
+			deferReply: true,
+			ephemeral: false,
+		});
 	},
 };
